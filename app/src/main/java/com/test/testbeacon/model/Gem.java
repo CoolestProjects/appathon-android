4eff957f45a823c8b0ac18c941e2bad42ea4a672
briangeoghegan2010@gmail.com <Briangeoghegan2010@gmail.com>
2017-03-25 18:28:26 +0000

    Gem Model added

---
+++ app/src/main/java/com/test/testbeacon/model/Gem.java
@@ -0,0 +1,20 @@
+
+
+
+class Gem{
+	private int greenGem = 1, private int redGem = 2, private int yellowGem = 3;
+	
+	public static void Gem(){
+	}
+
+	public static int getGreenGem(){
+		return this.greenGem;
+	}
+	public static int getRedGem(){
+		return this.redGem;
+	}
+	
+	public static int getYellowGem(){
+		return this.yellowGem;
+	}
+};
